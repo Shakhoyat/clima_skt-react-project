36b5d718c7c286b79018e752698ab571e2fd5d41
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-04 16:49:58 +0600

    feat: implement ThemeProvider for managing application theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { BrowserRouter } from "react-router-dom";
 import Layout from "@/components/layout";
+import { ThemeProvider } from "./context/theme-provider";
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>Hello</Layout>
+      <ThemeProvider defaultTheme="dark">
+        <Layout>Hello</Layout>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
