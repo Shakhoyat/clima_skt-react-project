f47d8790d04bea57dfc9b24c39a8fadbd67ff93d
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-04 23:07:23 +0600

    feat: update Header component to include dynamic logo and theme toggle functionality

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,23 @@
 import { Link } from "react-router-dom";
-
+import { useTheme } from "@/context/theme-provider";
+import { useState } from "react";
 const Header = () => {
+  const { theme, setTheme } = useTheme();
+  const isDark = theme === "dark";
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2  supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <Link to={"/"}>
-          <img src="/logo.png" alt="ClimaSKT " className="h-14" />
+          <img
+            src={isDark ? "/logo.png" : "/logo2.png"}
+            alt="ClimaSKT "
+            className="h-14"
+          />
         </Link>
-        <div>{/* theme toggle */}</div>
+        <div>
+          {/* theme toggle */}
+          <div onClick={() => setTheme(isDark ? "light" : "dark")}>Toggle</div>
+        </div>
       </div>
     </header>
   );
