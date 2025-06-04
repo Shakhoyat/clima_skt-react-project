2f3448a0b996f96cbc6a549231a1e8f73271a199
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-04 17:11:31 +0600

    feat: add routing and pages for WeatherDashboard and CityPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import { Button } from "@/components/ui/button";
 import { BrowserRouter } from "react-router-dom";
 import Layout from "@/components/layout";
 import { ThemeProvider } from "./context/theme-provider";
+import WeatherDashboard from "./pages/weather-dashboard";
+import CityPage from "./pages/city-page";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark">
-        <Layout>Hello</Layout>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<WeatherDashboard />} />
+            <Route path="/city/:cityName" element={<CityPage />} />
+          </Routes>
+        </Layout>
       </ThemeProvider>
     </BrowserRouter>
   );
