ea45c545e8fcc2bb7ebb40c7e5b7b4f666d8183b
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-05 17:14:17 +0600

    feat: add recharts and date-fns for hourly temperature visualization in HourlyTemperature component

--- src/components/hourly-temperature.tsx
+++ src/components/hourly-temperature.tsx
@@ -1,10 +1,72 @@
 import type { ForecastData } from "@/apis/types";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { LineChart, Line, ResponsiveContainer, XAxis, YAxis } from "recharts";
+import { format } from "date-fns";
 
 interface HourlyTemperatureProps {
   data: ForecastData; // Replace 'any' with the actual type of your hourly temperature data
 }
 const HourlyTemperature = ({ data }: HourlyTemperatureProps) => {
-  return <div>HourlyTemperature</div>;
+  const chartData = data.list.slice(0, 8).map((item) => ({
+    time: format(new Date(item.dt * 1000), "ha"),
+    temp: Math.round(item.main.temp),
+    feels_like: Math.round(item.main.feels_like),
+  }));
+  return (
+    <Card className="flex-1">
+      <CardHeader>
+        <CardTitle>Today's Temperature </CardTitle>
+        <CardDescription>Card Description</CardDescription>
+        <CardAction>Card Action</CardAction>
+      </CardHeader>
+      <CardContent>
+        <div className="h-[200px] w-full">
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart data={chartData}>
+              <XAxis
+                dataKey={"time"}
+                stroke="#8884d8"
+                fontSize={12}
+                tickLine={false}
+                axisLine={false}
+              />
+              <YAxis
+                stroke="#8884d8"
+                fontSize={12}
+                tickLine={false}
+                axisLine={false}
+                tickFormatter={(value) => `${value}°C`}
+              />
+
+              <Line
+                type="monotone"
+                dataKey="temp"
+                stroke="#8884d8"
+                strokeWidth={2}
+                dot={false}
+              />
+              <Line
+                type="monotone"
+                dataKey="feels_like"
+                stroke="#82ca9d"
+                strokeWidth={2}
+                dot={false}
+                strokeDasharray={"5 5"}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default HourlyTemperature;
