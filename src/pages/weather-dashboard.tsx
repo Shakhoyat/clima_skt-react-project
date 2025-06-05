24a87414ed5f64b6575cb7a0d27883fff0bd0891
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-05 12:41:02 +0600

    feat: refactor API configuration and improve WeatherAPI methods for better readability

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { RefreshCcw } from "lucide-react";
 
 const WeatherDashboard = () => {
-  return <div>WeatherDashboard</div>;
+  return (
+    <div className="p-4 space-y-4">
+      {/* Fav cities */}
+      <div className="flex items-center justify-between mb-4">
+        <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+        <Button
+          variant={"outline"}
+          size={"icon"}
+          // onClick={handleRefresh}
+          // disabled={true} // Disable button for now
+        >
+          <RefreshCcw className="h-4 w-4" />
+        </Button>
+      </div>
+      {/* current and hourly weather    */}
+    </div>
+  );
 };
 
 export default WeatherDashboard;
